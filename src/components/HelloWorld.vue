6f65064a124e047d79d38a026ca875ae1270ebee
robert <rsef899@aucklanduni.ac.nz>
2024-04-20 00:03:01 +1200

    added projects page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,19 @@
 <template>
-  <h1>Hello</h1>
+  
   <div class="h-screen w-full flex justify-center items-center bg-slate-50">
     <Enterance/>
   </div>
   <div class="w-full h-500px flex justify-center items-center py-20">
       <Skills/>
   </div>>
+  <div class="h-screen w-full flex justify-center items-center bg-slate-50">
+    <Projects/>
+  </div>
 </template>
 
 <script setup>
 import Enterance from './Enterance.vue'
 import Skills from './Skills.vue'
+import Projects from './Projects.vue'
 </script>
 
---
+++ src/components/Projects.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex flex-col">
+        <h2 class="text-green-700 font-googlePoppins font-bold text-lg lg:pt-5">My Projects</h2>
+    </div>
+
+
+</template>
